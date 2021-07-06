5844ceb4e223893c0fb82c1bdb4560f0520c087e
Alena Astravukh <helen151204@gmail.com>
2021-07-06 20:41:58 +0300

    feat: add menu page structure and content

--- apps/script.js
+++ apps/script.js
@@ -1,34 +1,43 @@
 'use strict';
 
 const wrapper = document.querySelector('.wrapper');
-wrapper.appendChild( createMainPage() );
+window.onload = () => iniStart();
 
-function createMainPage() {
-  const main_wrapper = document.createElement('div');
-  main_wrapper.className = 'main_wrapper';
+function iniStart() {
+  wrapper.appendChild( createMainPage() );
 
-  const main_button = document.createElement('div');
-  main_button.className = 'main_button';
-  main_wrapper.appendChild(main_button);
+  function createMainPage() {
+    const main_wrapper = document.createElement('div');
+    main_wrapper.className = 'main_wrapper';
 
-  const img = document.createElement('img');
-  img.src = 'assets/img/main/play_btn400.png';
-  img.className = 'btn_img';
-  img.alt = 'play_icon';
-  img.onclick = () => {
-    new Audio('../assets/sounds/click2.mp3').play()
-  };
-  // img.setAttribute("onclick","new Audio('../assets/sounds/click1.mp3').play()");
-  main_button.appendChild(img);
+    const main_button = document.createElement('div');
+    main_button.className = 'main_button';
+    main_wrapper.appendChild(main_button);
 
-  return main_wrapper;
+    const img = document.createElement('img');
+    img.src = 'assets/img/main/play_btn400.png';
+    img.className = 'btn_img';
+    img.alt = 'play_icon';
+    img.onclick = () => {
+      new Audio('../assets/sounds/click2.mp3').play()
+    };
+    // img.setAttribute("onclick","new Audio('../assets/sounds/click1.mp3').play()");
+    main_button.appendChild(img);
+
+    return main_wrapper;
+  }
+
+  const playButton = document.querySelector('.btn_img');
+  const main_wrapper = document.querySelector('.main_wrapper');
+  playButton.addEventListener('mousedown', () => playButton.style.transform = 'scale(0.9)');
+  playButton.addEventListener('mouseup', () => playButton.style.transform = 'scale(1.0)');
+  playButton.addEventListener('touchstart', () => playButton.style.transform = 'scale(0.9)');
+  playButton.addEventListener('touchend', () => playButton.style.transform = 'scale(1.0)');
+
+  playButton.addEventListener('click', () => console.log('Play the game!'));
+
+
+
+
+  playButton.addEventListener('click', () => main_wrapper.style.display = 'none');
 }
-
-const playButton = document.querySelector('.btn_img');
-playButton.addEventListener('mousedown', () => playButton.style.transform = 'scale(0.9)');
-playButton.addEventListener('mouseup', () => playButton.style.transform = 'scale(1.0)');
-playButton.addEventListener('touchstart', () => playButton.style.transform = 'scale(0.9)');
-playButton.addEventListener('touchend', () => playButton.style.transform = 'scale(1.0)');
-
-playButton.addEventListener('click', () => console.log('Play the game!'));
-
