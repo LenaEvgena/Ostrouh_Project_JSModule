9a0b04ad459470eeda4b20b7057b1f72aae7855a
Alena Astravukh <helen151204@gmail.com>
2021-07-16 13:48:01 +0300

    feat: add shapes drag n drop

--- apps/shapes.js
+++ apps/shapes.js
@@ -7,95 +7,219 @@ function initShapesGame() {
   const back_arrow = document.querySelector('.back_arrow');
   back_arrow.addEventListener('click', () => turnBack());
 
+  let dragImages = Array.from(document.querySelectorAll('.shape_image'));
+  let container = document.querySelector('.shapes_drop_images');
 
   let DraggedImage = null;
-  let DragShiftX;
-  let DragShiftY;
+  let shiftX;
+  let shiftY;
+  let isDragging = false;
 
+  dragImages.forEach((image) => {
+    image.addEventListener('mousedown', Drag_Start);
+  });
+
+    // let dropImages = {
+  //   cat: {
+  //     x: 40,
+  //     y: 0
+  //   },
+  //   cow: {
+  //     x: 600,
+  //     y: 0
+  //   },
+  //   dog: {
+  //     x: 300,
+  //     y: 50
+  //   },
+  //   horse: {
+  //     x: 450,
+  //     y: 150
+  //   },
+  //   lamb: {
+  //     x: 200,
+  //     y: 250
+  //   },
+  //   pig: {
+  //     x: 800,
+  //     y: 0
+  //   },
+  //   turkey: {
+  //     x: 700,
+  //     y: 150
+  //   },
+  //   update() {
+  //     for(let key in this) {
+  //       document.getElementById(key + '-shadow').style.position = 'absolute';
+  //       document.getElementById(key + '-shadow').style.left = this[key].x + 'px';
+  //       document.getElementById(key + '-shadow').style.top = this[key].y + 'px';
+  //     }
+  //   },
+  // }
+  // dropImages.update();
+  // setTimeout(() => {
+  //   getElementPos(dragImages);
+  // }, 500);
 
   function Drag_Start(EO) {
     // началось перетаскивание мячика
     EO = EO || window.event;
-    if (EO.which != 1) return;
     DraggedImage = EO.target;
 
-    DragShiftX = EO.pageX - DraggedImage.x;
-    DragShiftY = EO.pageY - DraggedImage.y;
+    if (EO.which != 1) return;
+    if (!DraggedImage) return;
+    EO.preventDefault();
 
-    DraggedImage.style.cursor = 'grabbing';
-    DraggedImage.style.zIndex = '1000';
-    document.addEventListener('mousemove', Drag_Move);
-    drag_container.addEventListener('mouseup', Drag_Stop);
+    DraggedImage.ondragstart = function() {
+      return false;
+    }
+
+    startDrag(DraggedImage, EO.clientX, EO.clientY);
   }
 
-  function Drag_Move(EO) {
+  function startDrag(element, clientX, clientY) {
+    if (isDragging) {
+      return;
+    }
+
+    isDragging = true;
+    document.addEventListener('mousemove', onMouseMove);
+    element.addEventListener('mouseup', onMouseUp);
+
+    shiftX = clientX - element.getBoundingClientRect().left;
+    shiftY = clientY - element.getBoundingClientRect().top;
+
+    element.style.position = 'absolute';
+    element.style.cursor = 'pointer';
+    element.style.zIndex = '1000';
+
+    wrapper.appendChild(element);
+
+    moveAt(clientX, clientY);
+  }
+
+  function moveAt(clientX, clientY) {
+    let newX = clientX - shiftX;
+    let newY = clientY - shiftY;
+
+    DraggedImage.style.left = newX + 'px';
+    DraggedImage.style.top = newY + 'px';
+  }
+  function onMouseUp(EO) {
     EO = EO || window.event;
     EO.preventDefault();
-    DraggedImage.style.left = (EO.pageX - DragShiftX) + "px";
-    DraggedImage.style.top = (EO.pageY - DragShiftY) + "px";
-
+    finishDrag();
+  }
+  function onMouseMove(EO) {
+    EO = EO || window.event;
+    EO.preventDefault();
+    moveAt(EO.clientX, EO.clientY);
     DraggedImage.hidden = true;
     let elemBelow = document.elementFromPoint(EO.clientX, EO.clientY);
     DraggedImage.hidden = false;
 
     if(!elemBelow) return;
-    if (DraggedImage.id === elemBelow.id) {
-      DivDrop();
+    if ((DraggedImage.id + '-shadow') === elemBelow.id) {
+      DivDrop(EO, elemBelow, container);
     }
   }
-
-  function Drag_Stop(EO) {
-    // закончилось перетаскивание мячика (неважно куда он уронен)
+  function DivDrop(EO, elemBelow, container) {
+      // мячик уронен
     EO = EO || window.event;
     EO.preventDefault();
-    DraggedImage.style.cursor = 'auto';
-    DraggedImage.style.zIndex = '1';
-    document.removeEventListener('mousemove', Drag_Move);
-    drag_container.removeEventListener('mouseup', Drag_Stop);
-    DraggedImage = null;
-  }
-
-  function DivDrop(EO) {
-    // мячик уронен
-    EO = EO || window.event;
-    EO.preventDefault();
-    if (DraggedImage){
-      drag_container.removeChild(DraggedImage);
-      DraggedImage.style.opacity = 0;
-      Drag_Stop();
+    if (DraggedImage) {
+      wrapper.removeChild(DraggedImage);
+      container.replaceChild(DraggedImage, elemBelow);//////
       taskIsDone();
     }
   }
+  function finishDrag() {
+    if(!isDragging) {
+      return;
+    }
 
-  function DivDragEnter(EO) {
-    EO = EO || window.event;
-    EO.preventDefault();
-    EO.currentTarget.style.transform = 'scale(1.1)';
+    isDragging = false;
+    DraggedImage.style.top = parseInt(DraggedImage.style.top) + pageYOffset + 'px';
+    DraggedImage.style.position = 'absolute';
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
   }
-  function DivDragOver(EO) {
-    EO = EO || window.event;
-    EO.preventDefault();
-  }
-  function DivDragLeave(EO) {
-    EO = EO || window.event;
-    EO.preventDefault();
-    EO.currentTarget.style.transform = 'scale(1.0)';
-  }
+  // function Drag_Move(EO) {
+  //   EO = EO || window.event;
+  //   EO.preventDefault();
+  //   DraggedImage.style.left = (EO.pageX - DragShiftX) + "px";
+  //   DraggedImage.style.top = (EO.pageY - DragShiftY) + "px";
+
+  //   DraggedImage.hidden = true;
+  //   let elemBelow = document.elementFromPoint(EO.clientX, EO.clientY);
+  //   DraggedImage.hidden = false;
+
+  //   if(!elemBelow) return;
+  //   if (DraggedImage.id === elemBelow.id) {
+  //     DivDrop();
+  //   }
+  // }
+
+  // function Drag_Stop(EO) {
+  //   // закончилось перетаскивание мячика (неважно куда он уронен)
+  //   EO = EO || window.event;
+  //   EO.preventDefault();
+  //   DraggedImage.style.cursor = 'auto';
+  //   DraggedImage.style.zIndex = '1';
+  //   document.removeEventListener('mousemove', Drag_Move);
+  //   dragImages.forEach((image) => {
+  //     image.removeEventListener('mouseup', Drag_Stop);
+  //   });
+  //   DraggedImage = null;
+  // }
+
+  // function DivDrop(EO) {
+  //   // мячик уронен
+  //   EO = EO || window.event;
+  //   EO.preventDefault();
+  //   if (DraggedImage){
+  //     shapes_drag_images.removeChild(DraggedImage);
+  //     DraggedImage.style.opacity = 0;
+  //     Drag_Stop();
+  //     taskIsDone();
+  //   }
+  // }
+
+  // function DivDragEnter(EO) {
+  //   EO = EO || window.event;
+  //   EO.preventDefault();
+  //   // EO.currentTarget.style.transform = 'scale(1.1)';
+  // }
+  // function DivDragOver(EO) {
+  //   EO = EO || window.event;
+  //   EO.preventDefault();
+  // }
+  // function DivDragLeave(EO) {
+  //   EO = EO || window.event;
+  //   EO.preventDefault();
+  //   // EO.currentTarget.style.transform = 'scale(1.0)';
+  // }
 
   function turnBack() {
     document.querySelector('.shapes_game_wrapper').style.display = 'none';
     back_arrow.style.transform = 'scale(0.9)';
+    back_arrow.style.cursor = 'pointer';
+
     document.querySelector('.menu_wrapper').style.display = 'flex';
   }
 
   function createShapesPage() {
     const imagesBase = `../assets/img/shapes/images/`;
     const shadowsBase = `../assets/img/shapes/shadows/`;
-    const images = ['cat.png', 'cow.png', 'dog.png', 'horse.png', 'lamb.png', 'pig.png', 'turkey.png'];
-    const shadows = ['cat-shadow.png', 'cow-shadow.png', 'dog-shadow.png', 'horse-shadow.png', 'lamb-shadow.png', 'pig-shadow.png', 'turkey-shadow.png'];;
+    const images = ['cat', 'cow', 'dog', 'horse', 'lamb', 'pig', 'turkey'];
+    const shadows = ['cat-shadow', 'cow-shadow', 'dog-shadow', 'horse-shadow', 'lamb-shadow', 'pig-shadow', 'turkey-shadow'];;
 
     const shapes_game_wrapper = document.createElement('div');
     shapes_game_wrapper.className = 'shapes_game_wrapper';
+
+    const buttons_container = document.createElement('div');
+    buttons_container.className = 'buttons_container';
 
     const drop_container = document.createElement('div');
     drop_container.className = 'drop_container';
@@ -103,32 +227,34 @@ function initShapesGame() {
     const drag_container = document.createElement('div');
     drag_container.className = 'drag_container';
 
-    shapes_game_wrapper.appendChild( createBackArrow() );
-    shapes_game_wrapper.appendChild( createTaskCheckPoint(7) );
 
-    drop_container.appendChild( createImage(shadowsBase, shadows, 'shadow_image', 'shapes_shadow_container', 'shapes_drop_images') );
-    drag_container.appendChild( createImage(imagesBase, images, 'shape_image', 'shapes_image_container', 'shapes_drag_images') );
+    buttons_container.appendChild( createBackArrow() );
+    buttons_container.appendChild( createTaskCheckPoint(7) );
+    shapes_game_wrapper.appendChild( buttons_container );
+
+    drop_container.appendChild( createImage(shadowsBase, shadows, 'shadow_image', 'shapes_drop_images') );
+    drag_container.appendChild( createImage(imagesBase, images, 'shape_image', 'shapes_drag_images') );
+    // drop_container.appendChild( createImage(shadowsBase, shadows, 'shadow_image', 'shapes_shadow_container', 'shapes_drop_images') );
+    // drag_container.appendChild( createImage(imagesBase, images, 'shape_image', 'shapes_image_container', 'shapes_drag_images') );
 
     shapes_game_wrapper.appendChild( createOverlay() );
 
-    shapes_game_wrapper.appendChild(drop_container);
     shapes_game_wrapper.appendChild(drag_container);
+    shapes_game_wrapper.appendChild(drop_container);
 
     return shapes_game_wrapper;
   }
 
-  function createImage(base, arr, className, boxClassName, parentClassName) {
+  function createImage(base, arr, className, parentClassName) {//boxClassName,
     const parent = document.createElement('div');
     parent.className = parentClassName;
     for (var i = 0; i < arr.length; i++) {
-      const box = document.createElement('div');
-      box.className = boxClassName;
       const img = document.createElement('img');
-      // img.id = id;
-      img.src = base + arr[i];
+      img.id = arr[i];
+      img.src = base + arr[i] + '.png';
       img.className = className;
-      box.appendChild(img);
-      parent.appendChild(box);
+      // box.appendChild(img);
+      parent.appendChild(img);
     }
     return parent;
   }
@@ -187,7 +313,7 @@ function initShapesGame() {
     return balloonsContainer;
   }
 
-  let tasksCount = 4;
+  let tasksCount = 7;
   function taskIsDone() {
     tasksCount--;
     const tasksPointsDiv = document.querySelector('#tasksPoints');
@@ -225,44 +351,11 @@ function initShapesGame() {
 
         setTimeout(() => {
           turnBack();
-          // document.querySelector('.colors_game_wrapper').style.display = 'none';
-          // document.querySelector('.menu_wrapper').style.display = 'flex';
         }, 7000)
-
 
       }, 100);
     }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
