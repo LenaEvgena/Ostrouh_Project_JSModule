ac9372649fbb442ee560aea00baba179bd21174c
Alena Astravukh <helen151204@gmail.com>
2021-07-31 15:45:00 +0300

    feat: add animation

--- apps/logic.js
+++ apps/logic.js
@@ -1,3 +1,42 @@
 'use strict';
+class AudioController {
+  constructor() {
+    this.bgMusic = new Audio('../assets/sounds/bgmusic.mp3');
+    this.slideSound = new Audio('../assets/sounds/slide.mp3');
+    this.clickSound = new Audio('../assets/sounds/click2.mp3');
+    this.bgMusic.volume = 0.03;
+    this.bgMusic.loop = true;
+  }
+  startMusic() {
+    this.bgMusic.play();
+  }
+  stopMusic() {
+    this.bgMusic.pause();
+    this.bgMusic.currentTime = 0;
+  }
+  clickSound() {
+    this.clickSound.play();
+  }
+  slideSound() {
+    this.slideSound.play();
+  }
+  //victory sound
+}
+
+function initLogicGame() {
+  let cards = Array.from(document.querySelectorAll('.card'));
+
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      //переворот карты
+
+    });
+  })
 
 
+
+
+
+}
+
+initLogicGame();
