b528cca5e44060c4bd3c1be955f9bcfa9d0c9f4a
Alena Astravukh <helen151204@gmail.com>
2021-08-01 20:26:04 +0300

    feat: add module

--- apps/logic.js
+++ apps/logic.js
@@ -1,44 +1,6 @@
 'use strict';
-class AudioController {
-  constructor() {
-    this.bgMusic = new Audio('../assets/sounds/bgmusic.mp3');
-    this.slide = new Audio('../assets/sounds/slide.mp3');
-    this.flip = new Audio('../assets/sounds/flip.mp3');
-    this.click = new Audio('../assets/sounds/click2.mp3');
-    this.balloonPop = new Audio('../assets/sounds/balloonpop.mp3');
-    this.hooray = new Audio('../assets/sounds/hooray.mp3');
-    this.cardPop = new Audio('../assets/sounds/cardpop.mp3');
-    // this.balloonPop.volume = 0.01;
-    this.bgMusic.volume = 0.03;
-    this.bgMusic.loop = true;
-  }
-  startMusic() {
-    this.bgMusic.play();
-  }
-  stopMusic() {
-    this.bgMusic.pause();
-    this.bgMusic.currentTime = 0;
-  }
-  clickSound() {
-    this.click.play();
-  }
-  flipSound() {
-    this.flip.play();
-  }
-  slideSound() {
-    this.slide.play();
-  }
-  balloonPopSound() {
-    this.balloon.play();
-  }
-  hooraySound() {
-    this.hooray.play();
-  }
-  cardPopSound() {
-    this.cardPop.play();
-  }
-}
 
+import { AudioController } from './AudioController.js';
 class MemoryGame {
   constructor(cards) {
     this.audioController = new AudioController();
@@ -53,7 +15,7 @@ class MemoryGame {
       this.isBusy = false;//можем начинать играть
     }, 500);
     // this.audioController.startMusic();
-    // this.shuffleCards(this.cardsArray);
+    this.shuffleCards(this.cardsArray);
   }
 
   endGame() {
