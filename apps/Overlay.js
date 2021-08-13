2757a376fab9ff72d6b65956274d52db8c544995
Alena Astravukh <helen151204@gmail.com>
2021-08-13 14:29:04 +0300

    feat: change win animation

--- apps/Overlay.js
+++ apps/Overlay.js
@@ -51,6 +51,7 @@ export class Overlay {
       EO.target.style.background = 'url(./assets/img/other/confetti.png)';
       EO.target.style.backgroundSize = 'cover';
       EO.target.style.width = '150px';
+
       globalThis.audioController.balloonPopSound();
 
       setTimeout(() => {EO.target.style.display = 'none'}, 300);
